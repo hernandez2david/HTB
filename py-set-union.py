090c6e6af204323848b78afb2c4a7ebd5d402aff
David Hernandez <davhernandez17@gmail.com>
2024-06-19 16:31:53 -0600

    Create py-set-union.py

---
+++ py-set-union.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/py-set-union/problem?isFullScreen=true
+
+s = set(input().split())
+r = int(input())
+n = set(input().split())
+z = int(input())
+print(len(s|n))
