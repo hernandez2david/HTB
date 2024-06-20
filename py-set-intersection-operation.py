aae22fef5f29a7207b012a2b6498a04fd108d766
David Hernandez <davhernandez17@gmail.com>
2024-06-20 16:37:26 -0600

    Create py-set-intersection-operation.py

---
+++ py-set-intersection-operation.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/py-set-intersection-operation/problem?isFullScreen=true
+#py-set-intersection-operation
+r = int(input())
+s = set(input().split())
+
+z = int(input())
+n = set(input().split())
+
+print(s&n)
